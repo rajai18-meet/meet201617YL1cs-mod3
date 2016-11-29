d897a94fde4ff21fa6b1af1dac5d43a2bd52f8b1
none <none>
2016-11-29 20:09:03 +0200

    Did not finish

---
+++ Student.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self,name,hometown,age,height, icecream):
+        print(self)
+
+    def print_summary(self):
+        print(self)
+
+    def get_giraffe_gap(self):
+        return (500-self)
+    

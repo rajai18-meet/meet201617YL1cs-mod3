dfb0d1bd0a7cc189b2c4a24590ffc78d62e22a8c
rajai18-meet <none>
2016-11-29 20:10:55 +0200

    Added test class for understanding concepts

---
+++ My_Class.py
@@ -0,0 +1,18 @@
+class MyClass:
+    """
+    test class for learning
+    """
+    def __init__(self,name):
+        self.name=name
+
+    def fun(self):
+        print(self.name)
+
+    def fun2(self,numtimes):
+        print(numtimes*self.name)
+    
+
+
+
+    def fun3(self,numtimes, user):
+        pass
